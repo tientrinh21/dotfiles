72f9629eb6dd9b15c015cbf2ae532ce97bef989c
Tien Trinh <57804513+tientrinh21@users.noreply.github.com>
2022-07-26 11:33:42 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,37 +6,53 @@ alias v="command vim"
 alias vim="nvim"
 alias ide="vim ."
 alias python="python3"
-alias ls="exa --group-directories-first --icons"
-alias ll="ls -l --git"
-alias la="ls -la --git"
+alias ls="exa --sort type --group-directories-first --icons"
+alias ll="ls -l --git --grid"
+alias la="ls -la --git --grid"
 alias g="git"
 alias gs="git status"
 alias ga="git add ."
 alias gc="git commit -m"
 alias gp="git push" 
+alias npm="pnpm"
+alias npx="pnpm dlx"
 
-# npm & vim plugin
-alias lsv="open http://localhost:8000/ && python -m http.server"
+
+# Live server & vim plugin
+alias lsv="open -inprivate http://localhost:8000/ && live-server --port=8000"
 alias f="floaterm"
 
-# WSL only
-alias exp="explorer.exe"
-alias open="powershell.exe /c start"
 
-function py -d "Run python script without typing the extension" 
-  command python3 $argv.py
+# Python
+function py -d "Run python script" 
+  command python3 $argv
 end
 
-function cl -d "Compile c/cpp files and run (without typing the extension)"
-  if test -e $argv.c
-    command gcc $argv.c -o $argv && ./$argv
-  else if test -e $argv.cpp
-    command g++ $argv.cpp -o $argv && ./$argv
-  end
+
+# C / C++
+function cl -d "Compile c/cpp files and run"
+	set --local extension (echo $argv | awk -F. '{print $NF}')
+	if test "cpp" = $extension
+		set -l out (string replace .cpp .out $argv) 
+    command g++ $argv -o $out && ./$out
+	else 
+		set -l out (string replace .c .out $argv) 
+		command gcc $argv -o $out && ./$out
+	end
 end
+
+
+# Java
+function jv -d "Run java project"
+  command javac $argv && java (string replace .java '' $argv)
+end
+
+export JDTLS_HOME=/home/tientrinh/.jdtls
+
 
 # Themes
 starship init fish | source
+
 
 # FZF
 export FZF_DEFAULT_COMMAND='fd --type file --color=always'
@@ -45,3 +61,19 @@ export FZF_DEFAULT_OPTS='-m --ansi --cycle --height 70% --layout reverse --info 
 # fzf.fish
 set fzf_preview_file_cmd bat --color=always --style=plain --line-range=:500 
 
+
+# pnpm
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+
+
+# WSL only
+alias see="explorer.exe"
+alias open="cmd.exe /c start msedge"
+
+# Window terminal (duplicate tab/pane)
+function storePathForWindowsTerminal --on-variable PWD
+    if test -n "$WT_SESSION"
+      printf "\e]9;9;%s\e\\" (wslpath -w "$PWD")
+    end
+end
